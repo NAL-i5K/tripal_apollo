1c076658d6a7238da35a8cd98eee4a2f1e0f2604
bradfordcondon <bradford.condon@gmail.com>
2018-10-17 19:53:10 -0400

    first stab

--- bin/set_travis_apollo.sh
+++ bin/set_travis_apollo.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
 
-curl-i -s -XPOST -H 'Content-Type: application/json' -d localhost:8888/addOrganism \
-{"username", : "", "password": "", }
-
-genus saccharomyces
-species cereviseae
-
-directory /data/yeast
+#use apollo API to create organism and expected groups
+curl -i -H 'Content-type: application/json' \
+  -X POST http://localhost:8888/organism/addOrganism -d  \
+  "{'username' : 'admin@local.host', 'password' : 'password', 'directory' : '/data/yeast',\
+  'commonName' : 'yeast', 'genus' : 'saccharomyces',\
+  'species' : 'cerevisiae'}"
+curl -i -H 'Content-type: application/json' \
+  -X POST http://localhost:8888/group/createGroup -d  \
+  "{'username' : 'admin@local.host', 'password' : 'password', 'name' : 'saccharomyces_cerevisiae_READ'"
+curl -i -H 'Content-type: application/json' \
+  -X POST http://localhost:8888/group/createGroup -d  \
+  "{'username' : 'admin@local.host', 'password' : 'password', 'name' : 'saccharomyces_cerevisiae_WRITE'"
