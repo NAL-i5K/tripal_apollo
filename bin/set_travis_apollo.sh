3cd2ccbb6e4a709629137c35f404e680edffd97f
bradfordcondon <bradford.condon@gmail.com>
2018-10-17 19:43:18 -0400

    init setup script and bring yeast files

---
+++ bin/set_travis_apollo.sh
@@ -0,0 +1,8 @@
+
+curl-i -s -XPOST -H 'Content-Type: application/json' -d localhost:8888/addOrganism \
+{"username", : "", "password": "", }
+
+genus saccharomyces
+species cereviseae
+
+directory /data/yeast
