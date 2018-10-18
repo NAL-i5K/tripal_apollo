4ebe0c290bfd2b7de91c7e69296400b803ae3971
bradfordcondon <bradford.condon@gmail.com>
2018-10-17 21:06:33 -0400

    cant execute python scripts in test

--- tests/tripal_apollo_api_Test.php
+++ tests/tripal_apollo_api_Test.php
@@ -23,4 +23,37 @@ class tripal_apollo_api_Test extends TripalTestCase {
 
   }
 
+  public function test_tripal_apollo_create_user_permissions(){
+    $url = 'localhost:8888';
+
+    $info = tripal_apollo_configureApollo($url);
+
+   $instance_info =  db_select('apollo_instance', 't')
+     ->fields('t')
+     ->condition('url', $url)
+     ->execute()
+     ->fetchObject();
+
+$user_info = ['uid' => NULL,
+  'name' => "walrus testaberger",
+  'pass' => 'some_unencrypted_password',
+  'email' => 'this_email_shouldnt_exist@never_gonna_happen.com',
+  'institution' => "U of Tripal"];
+
+   db_insert('apollo_user')
+     ->fields($user_info);
+
+$user_info['organism_key'] = $info['organism'];
+   //email
+    //name
+    //organism_key
+    //pass
+
+
+     tripal_apollo_create_user_permissions($user_info, $instance_info);
+
+     //TODO: write API call to fetch groups
+
+     $this->assertTrue(TRUE);
+    }
 }
