e9843cfb1b59c666cdb83efca8f6649b76b6df2f
bradfordcondon <bradford.condon@gmail.com>
2018-10-24 12:13:19 -0400

    missing schema

--- setup/set_travis_apollo.sh
+++ setup/set_travis_apollo.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+echo $APOLLO_URL
+
+
 #use apollo API to create organism and expected groups
 curl -i -H 'Content-type: application/json' \
-  -X POST $APOLLO_URL/organism/addOrganism -d  \
+  -X POST ${APOLLO_URL}/organism/addOrganism -d  \
   "{'username' : 'admin@local.host', 'password' : 'password', 'directory' : '/data/yeast',\
   'commonName' : 'yeast', 'genus' : 'saccharomyces',\
   'species' : 'cerevisiae'}"
 
 curl -i -H 'Content-type: application/json' \
-  -X POST $APOLLO_URL/group/createGroup -d  \
+  -X POST ${APOLLO_URL}/group/createGroup -d  \
   "{'username' : 'admin@local.host', 'password' : 'password', 'name' : 'saccharomyces_cerevisiae_USER'}"
 curl -i -H 'Content-type: application/json' \
-  -X POST $APOLLO_URL/group/createGroup -d  \
+  -X POST ${APOLLO_URL}/group/createGroup -d  \
   "{'username' : 'admin@local.host', 'password' : 'password', 'name' : 'saccharomyces_cerevisiae_WRITE'}"
